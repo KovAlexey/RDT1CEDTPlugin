4b490e72e29e2acb101277538b02a39fd2225c55
KovAlexey <kovalexei@gmail.com>
2024-03-06 00:11:21 +1000

    ИрОбщий.От для одного, двух объектов в выражениях (только плоско)

--- com.kovalexey.rdt1c.debug.ui/src/com/kovalexey/rdt1c/debug/ui/DebugCommandExecutor.java
+++ com.kovalexey.rdt1c.debug.ui/src/com/kovalexey/rdt1c/debug/ui/DebugCommandExecutor.java
@@ -2,6 +2,8 @@ package com.kovalexey.rdt1c.debug.ui;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,44 +16,136 @@ import com._1c.g5.v8.dt.debug.core.model.evaluation.IEvaluationListener;
 import com._1c.g5.v8.dt.debug.core.model.evaluation.IEvaluationRequest;
 import com._1c.g5.v8.dt.debug.core.model.evaluation.IEvaluationResult;
 import com._1c.g5.v8.dt.debug.core.model.values.BslValuePath;
+import com._1c.g5.v8.dt.debug.core.model.values.IBslValue;
 import com._1c.g5.v8.dt.debug.model.calculations.BaseValueInfoData;
 import com._1c.g5.v8.dt.debug.model.calculations.CalculationResultBaseData;
 import com._1c.g5.v8.dt.debug.model.calculations.ViewInterface;
+import com._1c.g5.v8.dt.platform.IEObjectTypeNames;
 
 public class DebugCommandExecutor {
+	public static final String TYPE_DATA_COMPOSITION_SCHEME_RU = "СхемаКомпоновкиДанных";
+	public static final String DATA_COMPOSITION_SETTINGS_RU = "НастройкиКомпоновкиДанных";
 	
-	public static void DebugThisVariable(IBslVariable var)
-	{
+	
+	
+	public static void DebugThisVariable(IBslStackFrame stackframe, String variable) {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("ИрОбщий.От(");
-		stringBuilder.append(var.toWatchExpression());
+		stringBuilder.append(variable);
 		stringBuilder.append(")");
 		
 		String executionCommand = stringBuilder.toString();
 		try {
-			EvaluateExpression(var.getStackFrame(), executionCommand);
-		}catch(DebugException e)
-		{
+			EvaluateExpression(stackframe, executionCommand);
+		} catch(DebugException e) {
 			// TODO: Обработка исключения
 			e.printStackTrace();
 		}
+	}
+	
+	public static void DebugThisVariable(IBslVariable var)
+	{
+		DebugThisVariable(var.getStackFrame(), var.toWatchExpression());
+	}
+	
+	public static void DebugVariables(IBslVariable[] variables) {
+		if (variables.length > 2) {
+			Notification.showMessage("Не поддерживается более двух параметров!");
+			return;
+		} else if (variables.length == 2) {
+			executeDebugDataCompositionSchemeFromSelected(variables);
+		} else if (variables.length == 1) {
+			DebugThisVariable(variables[0]);
+		}
+	}
+	
+	public static void DebugVariables(HashMap<String, IBslValue> variables) {
+		if (variables.size() > 2) {
+			Notification.showMessage("Не поддерживается более двух параметров!");
+			return;
+		} else if (variables.size() == 2) {
+			executeDebugDataCompositionSchemeFromSelected(variables);
+		} else if (variables.size() == 1) {
+			String expression = (String)variables.keySet().toArray()[0];
+			IBslValue value = variables.get(expression);
+			DebugThisVariable(value.getStackFrame(), expression);
+		}
+	}
+	
+	public static Boolean IsItBslValueDataComposition(IBslValue value) {
+		String typeName = value.getValueTypeName();
+		return (typeName.equals(TYPE_DATA_COMPOSITION_SCHEME_RU) 
+				|| typeName.equals(IEObjectTypeNames.DATA_COMPOSITION_SCHEMA));
+	}
+	
+	public static Boolean IsItBslValueDataCompositionSettings(IBslValue value) {
+		String typeName = value.getValueTypeName();
+		return (typeName.equals(DATA_COMPOSITION_SETTINGS_RU) 
+				|| typeName.equals(IEObjectTypeNames.DATA_COMPOSITION_SETTINGS));
+	}
+	
+	private static void executeDebugDataCompositionSchemeFromSelected(HashMap<String, IBslValue> variables) {
+		String variable_scheme = null, variable_settings = null;
+		IBslStackFrame stackFrame = null;
 		
+		Iterator<String> keys_iterator = variables.keySet().iterator();
+		while (keys_iterator.hasNext()) {
+			String expression = keys_iterator.next();
+			IBslValue value = variables.get(expression);
+			if (IsItBslValueDataComposition(value)) {
+				variable_scheme = expression;
+			} else if (IsItBslValueDataCompositionSettings(value)) {
+				variable_settings = expression;
+				stackFrame = value.getStackFrame();
+			}
+		}
+		
+		if (stackFrame != null && variable_scheme != null && variable_settings != null)
+		{
+			DebugDataCompostionScheme(stackFrame, variable_scheme, variable_settings);
+		} else {
+			Notification.showMessage("Выбранные значения не являются схемой компоновки данных с настройками!");
+		}
+	}
+	
+	private static void executeDebugDataCompositionSchemeFromSelected(IBslVariable[] variables) {
+		IBslVariable variable_scheme = null, variable_settings = null;
+		
+		for (IBslVariable object : variables) {
+			IBslValue value = object.getValue();
+			if (IsItBslValueDataComposition(value)) {
+				variable_scheme = object;
+			} else if (IsItBslValueDataCompositionSettings(value)) {
+				variable_settings = object;
+			}
+		}
+		
+		if (variable_scheme != null && variable_settings != null)
+		{
+			DebugDataCompostionScheme(variable_scheme, variable_settings);
+		} else {
+			Notification.showMessage("Выбранные значения не являются схемой компоновки данных с настройками!");
+		}
 	}
 	
 	public static void DebugDataCompostionScheme(IBslVariable scheme, IBslVariable settings)
 	{
+		DebugDataCompostionScheme(scheme.getStackFrame(), scheme.toWatchExpression(), settings.toWatchExpression());
+	}
+	
+	public static void DebugDataCompostionScheme(IBslStackFrame stackFrame, String scheme_expression, String settings_expression)
+	{
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("ИрОбщий.От(");
-		stringBuilder.append(scheme.toWatchExpression());
+		stringBuilder.append(scheme_expression);
 		stringBuilder.append(",");
-		stringBuilder.append(settings.toWatchExpression());
+		stringBuilder.append(settings_expression);
 		stringBuilder.append(")");
 		
 		String executionCommand = stringBuilder.toString();
 		try {
-			EvaluateExpression(scheme.getStackFrame(), executionCommand);
-		}catch(DebugException e)
-		{
+			EvaluateExpression(stackFrame, executionCommand);
+		} catch(DebugException e) {
 			// TODO: Обработка исключения
 			e.printStackTrace();
 		}
@@ -85,16 +179,14 @@ public class DebugCommandExecutor {
 
 		@Override
 		public void evaluationComplete(IEvaluationResult execution_result) throws DebugException {
-			if (!execution_result.isSuccess())
-			{
+			if (!execution_result.isSuccess()) {
 				return;
 			}
 			CalculationResultBaseData resultBaseData = execution_result.getResult();
-			if (resultBaseData.getErrorOccurred())
-			{
+			if (resultBaseData.getErrorOccurred()) {
 				byte[] exception = resultBaseData.getExceptionStr();
 				String result = new String(exception, StandardCharsets.UTF_8);
-				Notification.showmessage(expression, result);
+				Notification.showMessage(expression, result);
 				return;
 			}
 			BaseValueInfoData valueInfo = resultBaseData.getResultValueInfo();
@@ -115,7 +207,7 @@ public class DebugCommandExecutor {
 			builder.append("\r\n");
 			builder.append("Значение было скопировано в буфер обмена.");
 			
-			Notification.showmessage(expression, builder.toString());
+			Notification.showMessage(expression, builder.toString());
 		}
 		
 	}
---
+++ com.kovalexey.rdt1c.debug.ui/src/com/kovalexey/rdt1c/debug/ui/VariableViewDebugAction.java
@@ -0,0 +1,50 @@
+package com.kovalexey.rdt1c.debug.ui;
+
+import java.util.ArrayList;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+import com._1c.g5.v8.dt.debug.core.model.IBslVariable;
+
+public class VariableViewDebugAction implements IViewActionDelegate {
+	ISelection selection;
+
+	
+	
+	public VariableViewDebugAction() {
+	}
+
+	@Override
+	public void run(IAction action) {
+		if (!selection.isEmpty()
+				&& selection instanceof TreeSelection)
+		{
+			TreeSelection var_selection = (TreeSelection)selection;
+			
+			Object[] objects = var_selection.toArray();
+			ArrayList<IBslVariable> variables = new ArrayList<IBslVariable>();
+			for (Object object: objects) {
+				if (object instanceof IBslVariable) {
+					variables.add((IBslVariable)object);
+				}
+			}
+			
+			DebugCommandExecutor.DebugVariables(variables.toArray(size -> new IBslVariable[size]));
+		}
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+	@Override
+	public void init(IViewPart view) {
+	}
+
+}
