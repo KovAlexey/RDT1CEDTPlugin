0f83d7b6acdf9be3170d31235f95fbcc11e44d1c
KovAlexey <kovalexei@gmail.com>
2024-04-01 00:00:06 +1000

    Открытие редактора по URI объекта

--- bundles/com.kovalexey.rdt1c.debug.ui/src/com/kovalexey/rdt1c/debug/ui/ExternalDependeciesInjector.java
+++ bundles/com.kovalexey.rdt1c.debug.ui/src/com/kovalexey/rdt1c/debug/ui/ExternalDependeciesInjector.java
@@ -1,8 +1,13 @@
 package com.kovalexey.rdt1c.debug.ui;
 
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
 
+import com._1c.g5.v8.dt.core.platform.IBmModelManager;
+import com._1c.g5.v8.dt.core.platform.IConfigurationProvider;
+import com._1c.g5.v8.dt.core.platform.IDtProjectManager;
 import com._1c.g5.v8.dt.core.platform.IV8ProjectManager;
+import com._1c.g5.v8.dt.core.xtext.resource.ExtendedResourceServiceProvider;
 import com._1c.g5.wiring.AbstractServiceAwareModule;
 
 public class ExternalDependeciesInjector extends AbstractServiceAwareModule {
@@ -16,6 +21,10 @@ public class ExternalDependeciesInjector extends AbstractServiceAwareModule {
 	protected void doConfigure() {
 		// TODO Auto-generated method stub
 		bind(IV8ProjectManager.class).toService();
+		bind(IBmModelManager.class).toService();
+		bind(IDtProjectManager.class).toService();
+		bind(IConfigurationProvider.class).toService();
+		
 	}
 
 }
